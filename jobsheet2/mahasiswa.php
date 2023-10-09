ff779e810f3405ac9d17bebd2cec7db042a3ca78
Farah Nada Syahidah <134130282+farahnada@users.noreply.github.com>
2023-10-09 22:04:54 +0700

    Create mahasiswa.php

---
+++ jobsheet2/mahasiswa.php
@@ -0,0 +1,31 @@
+<?php
+
+//membuat class
+class mahasiswa
+{
+    //menuliskan properti
+    var $nim;
+    var $nama;
+    var $alamat;
+
+    //method untuk menampilkan nama
+    function tampil_nama()
+    {
+        return "Nama saya adalah Farah";
+    }
+
+    //method untuk menampilkan alamat
+    function tampil_alamat()
+    {
+        //isi method
+        return "Alamat saya di Desa Rowokele";
+    }
+}
+
+//membuat objek nama_mahasiswa
+$nama_mahasiswa = new mahasiswa();
+
+//menampilkan objek ke layar
+echo $nama_mahasiswa->tampil_nama();
+echo "</br>";
+echo $nama_mahasiswa->tampil_alamat();
