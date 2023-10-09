c252bf45f3652b38e3d89ec0df133fce50034abc
Farah Nada Syahidah <134130282+farahnada@users.noreply.github.com>
2023-10-09 22:04:38 +0700

    Update variables.php

--- jobsheet1/variables.php
+++ jobsheet1/variables.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Variables</title>
 </head>
+
 <body>
-    
+    <?php
+    $integer = 25;
+    $string = "Praktikum Web II";
+    $boolean = true;
+
+    echo "Ini contoh integer $integer </br>";
+    echo "Ini contoh string $string </br>";
+    echo "Ini contoh boolean $boolean </br>";
+    ?>
 </body>
+
 </html>
