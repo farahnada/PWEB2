5c6fb6304f8ca0b9d4c03a57f8fedca0178cb42c
Farah Nada Syahidah <134130282+farahnada@users.noreply.github.com>
2023-10-09 22:04:57 +0700

    Create public.php

---
+++ jobsheet2/public.php
@@ -0,0 +1,29 @@
+<?php
+
+//membuat class mahasiswa
+class mahasiswa
+{
+    //properti public dan private 
+    public $nama;
+    private $nim = "220302034";
+
+    //public method
+    public function tampilkan_nama()
+    {
+        //nilai kembalian
+        return "Nama Saya Farah </br>";
+    }
+
+    //mmbuat protected method
+    function tampilkan_nim()
+    {
+        return "NIM saya " . $this->nim;
+    }
+}
+
+//instansiasi objek mahasiswa ke dalam variabel $mahasiswa
+$mahasiswa = new mahasiswa();
+
+//memanggil method tampilkan_nama
+echo $mahasiswa->tampilkan_nama();
+echo $mahasiswa->tampilkan_nim();
