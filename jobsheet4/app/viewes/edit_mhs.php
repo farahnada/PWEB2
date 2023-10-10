b54145689ac82b61987858f0813fcbc05af72185
Farah Nada Syahidah <134130282+farahnada@users.noreply.github.com>
2023-10-10 21:14:21 +0700

    jobsheet 4

---
+++ jobsheet4/app/viewes/edit_mhs.php
@@ -0,0 +1,75 @@
+<?php
+include '../classes/database.php';
+$db = new database();
+?>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Edit Mahasiswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<div class="px-3 py-3">
+    <h3>Edit Data Mahasiswa</h3>
+    <a class="btn btn-primary" href="tampil_mhs.php">Kembali</a>
+    <br><br>
+    <!-- <form action="proses_mhs.php?aksi=update" method="post">
+    <?php
+    foreach ($db->edit($_GET['id']) as $d) {
+    ?>
+        <table>
+            <tr>
+                <td>NIM</td>
+                <td>
+                    <input type="hidden" name="id" value="<?php echo $d['id'] ?>">
+                    <input type="text" name="nim" value="<?php echo $d['nim'] ?>">
+                </td>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td><input type="text" name="nama" value="<?php echo $d['nama'] ?>"></td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>
+                    <textarea name="alamat" cols="30" rows="5"><?php echo $d['alamat'] ?></textarea>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input class="btn btn-success" type="submit" value="simpan"></td>
+            </tr>
+        </table>
+    <?php
+    }
+    ?>
+</form> -->
+
+    <div style="width: 30%;">
+        <form action="proses_mhs.php?aksi=update" method="post">
+            <?php
+            foreach ($db->edit($_GET['id']) as $d) {
+            ?>
+                <div class="mb-3">
+                    <label for="disabledTextInput" class="form-label">NIM</label>
+                    <input type="hidden" id="disabledTextInput" name="id" class="form-control" placeholder="" value="<?php echo $d['id'] ?>">
+                    <input type="text" id="disabledTextInput" name="nim" class="form-control" placeholder="" value="<?php echo $d['nim'] ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="disabledTextInput" class="form-label">Nama</label>
+                    <input type="text" id="disabledTextInput" name="nama" class="form-control" placeholder="" value="<?php echo $d['nama'] ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="disabledTextInput" class="form-label">Alamat</label>
+                    <textarea id="disabledTextInput" name="alamat" cols="30" rows="5" class="form-control" placeholder=""><?php echo $d['alamat'] ?></textarea>
+                </div>
+                <tr>
+                    <td><input class="btn btn-success" type="submit" value="simpan" </td>
+                </tr>
+            <?php
+            }
+            ?>
+        </form>
+    </div>
+</div>
---
+++ jobsheet4/app/viewes/tampil_mhs.php
@@ -0,0 +1,96 @@
+<?php
+
+include '../classes/database.php';
+//instansiasi class database
+$db = new database;
+?>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SIAKAD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<div class="px-3 py-3">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">SIAKAD</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Mahasiswa</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Dosen</a>
+                    </li>
+
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <!-- alert -->
+    <?php
+    if (isset($_GET['pesan']) && $_GET['pesan'] == 'berhasil1') { ?>
+        <div class="alert alert-success mx-auto text-center" style="width:18%" role="alert">
+            Data berhasil<a href="#" class="alert-link"> DITAMBAHKAN</a>
+        </div>
+    <?php
+        header("refresh:2,url=tampil_mhs.php");
+    } elseif (isset($_GET['pesan']) && $_GET['pesan'] == 'berhasil2') { ?>
+        <div class="alert alert-warning mx-auto text-center" style="width:15%" role="alert">
+            Data berhasil<a href="#" class="alert-link"> DIEDIT</a>
+        </div>
+    <?php
+        header("refresh:2,url=tampil_mhs.php");
+    } elseif (isset($_GET['pesan']) && $_GET['pesan'] == 'berhasil3') { ?>
+        <div class="alert alert-danger mx-auto text-center" style="width:15%" role="alert">
+            Data berhasil <a href="#" class="alert-link"> DIHAPUS</a>
+        </div>
+    <?php
+        header("refresh:2,url=tampil_mhs.php");
+    }
+    ?>
+
+    <div class="px-5">
+        <h3>Data Mahasiswa</h3>
+        <a class="btn btn-primary mb-2 mt-2" href="input_mhs.php">Tambah Mahasiswa</a>
+        <br><br>
+        <table class="table table-striped table-bordered" style="text-align:center">
+            <tr>
+                <th>No</th>
+                <th>NIM</th>
+                <th>Nama</th>
+                <th>Alamat</th>
+                <th>Aksi</th>
+            </tr>
+            <?php
+            $no = 1;
+            foreach ($db->tampil_mahasiswa() as $x) {
+            ?>
+                <tr>
+                    <td><?php echo $no++ ?></td>
+                    <td><?php echo $x['nim'] ?></td>
+                    <td><?php echo $x['nama'] ?></td>
+                    <td><?php echo $x['alamat'] ?></td>
+                    <td>
+                        <a class="btn btn-warning" href="edit_mhs.php?id=<?php echo $x['id']; ?>&aksi=edit">Edit</a>
+                        <a class="btn btn-danger" href="proses_mhs.php?id=<?php echo $x['id']; ?>&aksi=hapus">Hapus</a>
+                    </td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
+    </div>
+</div>
